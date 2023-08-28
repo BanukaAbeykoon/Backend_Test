fe4f0fc3790bfe95d2c7a1101f2b1bfa1c94bec2
BanukaAbeykoon <it20203658@my.sliit.lk>
2023-08-28 17:01:35 +0530

    Hirushan

--- New folder (3)/run.py
+++ New folder (3)/run.py
@@ -1,22 +1,20 @@
 import csv
 from flask import Flask, jsonify, request
-import numpy as np
-import pandas as pd
 from flask_pymongo import PyMongo
 from waitress import serve
-import numpy as np 
+import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt 
-from surprise import Dataset,Reader
-from surprise import SVDpp
 import seaborn as sns
 from surprise import Dataset,Reader
 from surprise import SVDpp
 from sklearn.preprocessing import StandardScaler
-# to suppress warnings 
+import requests
+# to suppress warnings
 from warnings import filterwarnings
 filterwarnings('ignore')
+
 
 
 app = Flask(__name__)
@@ -29,23 +27,6 @@ csv_file_path = "C:/Users/BANU/Downloads/google_review_ratings.csv"
 # mongodb database
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
-
-
-@app.route('/lastrecode', methods=['GET'])
-def handle_api():
-        last_record = db.placesflask.find_one(sort=[('_id', -1)])
-
-        if last_record:
-            # Convert the record's _id to a string
-            last_record['_id'] = str(last_record['_id'])
-            
-            # Return the last record as a JSON response
-            return jsonify({'placesflask': last_record})
-
-        # Return an appropriate response if there are no records
-        return jsonify({'message': 'No records found'})
-
-
 
 
 @app.route('/code', methods=['POST'])
@@ -399,9 +380,10 @@ def get_csv_record_count():
     except Exception as e:
         return jsonify({"error": str(e)})
     
-    
+    # Lahiru
+
 @app.route('/apiii', methods=['POST'])
-def run_algorithm():
+def run_algorithmm():
     # Get request parameters
     data = request.get_json()
     hotelCityy = data.get('hotelCity')
@@ -425,11 +407,11 @@ def run_algorithm():
 def mainn(hotelCityy,hotelPricee):
     hotelCityy=hotelCityy
     hotelPricee=hotelPricee
-    data = pd.read_excel('/home/ubuntu/Backend_Test/New folder (3)/data_hotel.xlsx')
+    data = pd.read_excel('C:/Users/BANU/OneDrive - Sri Lanka Institute of Information Technology/Desktop/Backendtest/Backend_Test/New folder (3)/data_hotel.xlsx')
     data['hotel_description'] = data['hotel_description'].fillna('tidak ada deskripsi')
     data[data['price_per_night'].isnull()]
     data.fillna(method='ffill',axis=0,inplace=True)
-    review = pd.read_excel('/home/ubuntu/Backend_Test/New folder (3)/review_hotel.xlsx')
+    review = pd.read_excel('C:/Users/BANU/OneDrive - Sri Lanka Institute of Information Technology/Desktop/Backendtest/Backend_Test/New folder (3)/review_hotel.xlsx')
     review[review['hotel_id'].isnull()]
     review.dropna(inplace=True)
     review_piv = review.pivot_table(index='hotel_id', aggfunc={'stay_duration':'mean','adults':'mean','children':'mean','rating':['mean','count']})
@@ -509,6 +491,723 @@ def handle_displayy():
         # Return an appropriate response if there are no records
         return jsonify({'message': 'No records found'})
 
+
+# hirushan
+
+@app.route('/lastrecodee', methods=['GET'])
+def handle_api():
+        last_record = db.routesflask.find_one(sort=[('_id', -1)])
+        if last_record:
+            last_record['_id'] = str(last_record['_id'])
+            return jsonify({'routesflask': last_record})
+        return jsonify({'message': 'No records found'})
+
+@app.route('/lastlocation', methods=['GET'])
+
+def handle_lastlocation():
+        last_record = db.locationsflask.find_one(sort=[('_id', -1)])
+
+        if last_record:
+
+            last_record['_id'] = str(last_record['_id'])
+
+            return jsonify({'locationsflask': last_record})
+        return jsonify({'message': 'No records found'})
+
+
+@app.route('/api', methods=['POST'])
+
+def run_algorithm():
+
+    # Get request parameters
+
+    data = request.get_json()
+
+    start_lat = data.get('StartLat')
+
+    start_lon = data.get('StartLon')
+
+    end_lat = data.get('EndLat')
+
+    end_lon = data.get('EndLon')
+
+    temple = data.get('Temples')
+
+    heritages = data.get('Heritages')
+
+    beaches = data.get('Beaches')
+
+    parks = data.get('Parks')
+
+    arts = data.get('Arts')
+
+    username = data.get('Username')
+
+ 
+
+    # Call main function
+
+    result = main(start_lat, start_lon, end_lat, end_lon, temple, heritages, beaches, parks, arts)
+
+ 
+
+    routeww = {
+
+        'userName': username,
+
+        'result': result
+
+    }
+
+ 
+
+    # Save result to MongoDB
+
+    db.routesflask.insert_one(routeww)
+
+ 
+
+    finalresult = result
+
+ 
+
+    newresult = get_locations(finalresult)
+
+ 
+
+    routewww = {
+
+        'userName': username,
+
+        'places' : newresult
+
+    }
+
+ 
+
+    # Save result to MongoDB
+
+    db.locationsflask.insert_one(routewww)
+
+ 
+
+    return 'Success'
+
+ 
+
+ 
+
+ 
+
+def main(StartLat, StartLon, EndLat, EndLon, Temples, Heritages, Beaches, Parks, Arts):
+
+    # print(StartLat)
+
+    # print(StartLon)
+
+    # print(EndLat)
+
+    # print(EndLon)
+
+    # print(Temples)
+
+    # print(Heritages)
+
+    # print(Beaches)
+
+    matrixTemple = np.zeros((1, 2))
+
+    rowsTemple = ['Avarage Distance']
+
+    columnsTemple = ['Japanese Peace Pagoda', 'Shri Sudharmalaya Buddhist Temple\t']
+
+    df1 = pd.DataFrame(matrixTemple, index=rowsTemple, columns=columnsTemple)
+
+   
+
+    matrixHeritageAndHistoricalPlaces = np.zeros((1, 7))
+
+    rowsHeritagesHeritageAndHistoricalPlaces = ['Avarage Distance']
+
+    columnsHeritageHeritageAndHistoricalPlaces = ['Galle Fort', 'Galle Fort Lighthouse\t', 'Dutch Reformed Church', 'Old Dutch Market\t', 'Dutch Hospital Shopping Precinct\t', 'Old Gate', 'Martin Wickramasinghe House & Folk Museum']
+
+    df2 = pd.DataFrame(matrixHeritageAndHistoricalPlaces, index=rowsHeritagesHeritageAndHistoricalPlaces, columns=columnsHeritageHeritageAndHistoricalPlaces)
+
+   
+
+    matrixBeaches = np.zeros((1, 7))
+
+    rowsBeaches = ['Avarage Distance']
+
+    columnsBeaches = ['Jungle Beach', 'Unawatuna Beach', 'Hikkaduwa Beach', 'Dalawella Beach', 'Talpe Beach', 'Ahungalla Beach', 'Induruwa Beach']
+
+    df3 = pd.DataFrame(matrixBeaches, index=rowsBeaches, columns=columnsBeaches)
+
+ 
+
+    matrixParks = np.zeros((1, 4))
+
+    rowsParks = ['Avarage Distance']
+
+    columnsParks = ['Beach Park Galle Municipal Council', 'Mahamodara Beach Park Marine Walk', 'The Great wall of lovers galle', 'New Marine Walk Galle Sea sight']
+
+    df4 = pd.DataFrame(matrixParks, index=rowsParks, columns=columnsParks)
+
+   
+
+    matrixArtGallary = np.zeros((1, 2))
+
+    rowsArtGallaries = ['Avarage Distance']
+
+    columnsArtGallaries = ['The Galle Fort Art Gallery', 'Pradeep Pencil Art Gallary']
+
+    df5 = pd.DataFrame(matrixArtGallary, index=rowsArtGallaries, columns=columnsArtGallaries)
+
+    # print("abc")
+
+    data = pd.read_csv("C:/Users/BANU/OneDrive - Sri Lanka Institute of Information Technology/Desktop/Backendtest/Backend_Test/New folder (3)/DataSet.csv")
+
+   
+
+    # Temples = 3.0
+
+    # Heritages = 4.0
+
+    # Beaches = 6.0
+
+    # StartLat = 6.0265776
+
+    # StartLon = 80.21862858
+
+    # EndLat = 5.9477618
+
+    # EndLon = 80.4519634
+
+    count1 = 0
+
+    count2 = 0
+
+    count3 = 0
+
+    count4 = 0
+
+    count5 = 0
+
+    start_positions = []
+
+    final_start_positions = []
+
+    time = 12
+
+    start_positions.append(StartLat)
+
+    start_positions.append(StartLon)
+
+ 
+
+    while time > 0 and (Temples > 0 or Heritages > 0 or Beaches > 0 or Parks > 0 or Arts > 0):
+
+        ProbArr = [Temples, Heritages, Beaches, Parks, Arts]
+
+ 
+
+        if getMaxValue(ProbArr) == Temples:
+
+            Temples *= 0.75
+
+ 
+
+            if count1 == 0:
+
+                findNeares(matrixTemple, StartLat, StartLon, EndLat, EndLon, df1)
+
+                count1 +=1
+
+               
+
+            column_name_temple = df1.columns[find_min_value_position(matrixTemple)]
+
+           
+
+            galle_data_temple = data[data['Places'] == column_name_temple]
+
+            latitude_col_temple = galle_data_temple['Latitude'].values[0]
+
+            longitude_col_temple = galle_data_temple['Longitude'].values[0]
+
+            start_positions.append(latitude_col_temple)
+
+            start_positions.append(longitude_col_temple)
+
+            # StartLat = latitude_col_temple
+
+            # StartLon = longitude_col_temple
+
+           
+
+            time -= 1
+
+        elif getMaxValue(ProbArr) == Heritages:
+
+            Heritages *= 0.75
+
+           
+
+            if count2 == 0:
+
+                findNeares(matrixHeritageAndHistoricalPlaces, StartLat, StartLon, EndLat, EndLon, df2)
+
+                count2 += 1
+
+           
+
+            column_name_HeritageAndHistoricalPlaces = df2.columns[find_min_value_position(matrixHeritageAndHistoricalPlaces)]
+
+           
+
+            galle_data_column_name_HeritageAndHistoricalPlaces = data[data['Places'] == column_name_HeritageAndHistoricalPlaces]
+
+            latitude_col_HeritageAndHistoricalPlaces = galle_data_column_name_HeritageAndHistoricalPlaces['Latitude'].values[0]
+
+            longitude_col_HeritageAndHistoricalPlaces = galle_data_column_name_HeritageAndHistoricalPlaces['Longitude'].values[0]
+
+            start_positions.append(latitude_col_HeritageAndHistoricalPlaces)
+
+            start_positions.append(longitude_col_HeritageAndHistoricalPlaces)
+
+            # StartLat = latitude_col_HeritageAndHistoricalPlaces
+
+            # StartLon = longitude_col_HeritageAndHistoricalPlaces
+
+ 
+
+           
+
+            time -= 1
+
+        elif getMaxValue(ProbArr) == Beaches:
+
+            Beaches *= 0
+
+           
+
+            if count3 == 0:
+
+                findNeares(matrixBeaches, StartLat, StartLon, EndLat, EndLon, df3)
+
+                count3 += 1
+
+           
+
+           
+
+            column_name_Beaches = df3.columns[find_min_value_position(matrixBeaches)]
+
+           
+
+            galle_data_column_name_Beaches = data[data['Places'] == column_name_Beaches]
+
+            latitude_col_Beaches = galle_data_column_name_Beaches['Latitude'].values[0]
+
+            longitude_col_Beaches = galle_data_column_name_Beaches['Longitude'].values[0]
+
+            start_positions.append(latitude_col_Beaches)
+
+            start_positions.append(longitude_col_Beaches)
+
+ 
+
+            # StartLat = latitude_col_Beaches
+
+            # StartLon = longitude_col_Beaches
+
+           
+
+           
+
+            time -= 4
+
+        elif getMaxValue(ProbArr) == Parks:
+
+            Parks *= 0
+
+           
+
+            if count4 == 0:
+
+                findNeares(matrixParks, StartLat, StartLon, EndLat, EndLon, df4)
+
+                count4 += 1
+
+           
+
+            column_name_Parks = df4.columns[find_min_value_position(matrixParks)]
+
+           
+
+            galle_data_column_name_Parks = data[data['Places'] == column_name_Parks]
+
+            latitude_col_Parks = galle_data_column_name_Parks['Latitude'].values[0]
+
+            longitude_col_Parks = galle_data_column_name_Parks['Longitude'].values[0]
+
+            start_positions.append(latitude_col_Parks)
+
+            start_positions.append(longitude_col_Parks)
+
+           
+
+            time -= 2
+
+       
+
+ 
+
+        else:
+
+            Arts *= 0.75
+
+           
+
+            if count5 == 0:
+
+                findNeares(matrixArtGallary, StartLat, StartLon, EndLat, EndLon, df5)
+
+                count5 += 1
+
+           
+
+            column_name_ArtGallary = df5.columns[find_min_value_position(matrixArtGallary)]
+
+           
+
+            galle_data_column_name_ArtGallary = data[data['Places'] == column_name_ArtGallary]
+
+            latitude_col_ArtGallary = galle_data_column_name_ArtGallary['Latitude'].values[0]
+
+            longitude_col_ArtGallary = galle_data_column_name_ArtGallary['Longitude'].values[0]
+
+            start_positions.append(latitude_col_ArtGallary)
+
+            start_positions.append(longitude_col_ArtGallary)
+
+           
+
+            time -= 2
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+    # start_positions.append(EndLat)
+
+    # start_positions.append(EndLon)
+
+    #.................................................................................
+
+ 
+
+    list_size = len(start_positions)
+
+    arr_size = list_size//2
+
+    # print(arr_size)
+
+    matrixNew = np.zeros((arr_size, arr_size))
+
+    # print(matrixNew)
+
+   
+
+ 
+
+    # for i in range(len(start_positions)):
+
+    #     print(start_positions[i], end=" ")
+
+ 
+
+    newlen = len(start_positions) // 2
+
+ 
+
+    arrz = [[0 for _ in range(newlen)] for _ in range(newlen)]
+
+    for i in range(1):
+
+        for j in range(1 + i, newlen):
+
+           
+
+            url3 = "http://router.project-osrm.org/route/v1/driving/" + str(start_positions[i + i + 1]) + "," + str(start_positions[i + i]) + ";" + str(start_positions[j + j + 1]) + "," + str(start_positions[j + j]) + "?overview=false"
+
+            response3 = requests.get(url3).json()
+
+            distance3 = response3["routes"][0]["distance"]
+
+           
+
+            arrz[i][j] = distance3
+
+           
+
+#             arrz[i][j] = start_positions[i + i] + start_positions[i + i + 1] + start_positions[j + j] + start_positions[j + j + 1]
+
+            #arrz[j][i] = arrz[i][j]
+
+#             print("The shortest road distance between", row_name, "and", column_name, "is", distance1, "meters.")
+
+ 
+
+ 
+
+    minValue = 0
+
+    # for row in arrz:
+
+    #     for element in row:
+
+    #         print(element, end=" ")
+
+    #     print()  
+
+ 
+
+    for j in range(newlen):
+
+        minIndex = j
+
+        for k in range(j + 1, newlen):
+
+            if arrz[0][k] < arrz[0][minIndex]:
+
+                minIndex = k
+
+        # Swap elements in the sorted array
+
+        arrz[0][j], arrz[0][minIndex] = arrz[0][minIndex], arrz[0][j]
+
+        # Swap corresponding j values in the unsorted array
+
+        start_positions[j + j], start_positions[minIndex + minIndex] = start_positions[minIndex + minIndex], start_positions[j + j]
+
+        start_positions[j + j + 1], start_positions[minIndex + minIndex + 1] = start_positions[minIndex + minIndex + 1], start_positions[j + j + 1]
+
+ 
+
+    # Print the sorted first row and corresponding j values from the unsorted array
+
+    # print("Sorted first row:")
+
+    for j in range(newlen):
+
+        # print(arrz[0][j], "corresponding j value:", start_positions[j + j])
+
+        # print(arrz[0][j], "corresponding j value:", start_positions[j + j + 1])
+
+        final_start_positions.append(start_positions[j + j])
+
+        final_start_positions.append(start_positions[j + j + 1])
+
+   
+
+    # print(final_start_positions)
+
+ 
+
+    #.................................................................................
+
+    final_start_positions.append(EndLat)
+
+    final_start_positions.append(EndLon)
+
+    # print(EndLat)
+
+    # print(EndLon)
+
+    return final_start_positions
+
+ 
+
+def getMaxValue(ProbArr):
+
+    max_value = ProbArr[0]
+
+    for value in ProbArr:
+
+        if value > max_value:
+
+            max_value = value
+
+    return max_value
+
+ 
+
+def findNeares(disArr, StartLat, StartLon, EndLat, EndLon, df):
+
+    data = pd.read_csv("C:/Users/BANU/OneDrive - Sri Lanka Institute of Information Technology/Desktop/Backendtest/Backend_Test/New folder (3)/DataSet.csv")
+
+    for i in range(1):
+
+        for j in range(len(disArr[0])):
+
+            column_name = df.columns[j]
+
+            galle_data_column = data[data['Places'] == column_name]
+
+            latitude_col = galle_data_column['Latitude'].values[0]
+
+            longitude_col = galle_data_column['Longitude'].values[0]
+
+           
+
+            row_name = df.index[i]
+
+     
+
+            import requests
+
+            import json
+
+ 
+
+            url1 = "http://router.project-osrm.org/route/v1/driving/" + str(longitude_col) + "," + str(latitude_col) + ";" + str(StartLon) + "," + str(StartLat) + "?overview=false"
+
+            response1 = requests.get(url1).json()
+
+            distance1 = response1["routes"][0]["distance"]
+
+             
+
+           
+
+            url2 = "http://router.project-osrm.org/route/v1/driving/" + str(longitude_col) + "," + str(latitude_col) + ";" + str(EndLon) + "," + str(EndLat) + "?overview=false"
+
+            response2 = requests.get(url2).json()
+
+            distance2 = response2["routes"][0]["distance"]
+
+           
+
+            distence = distance1 + distance2
+
+            disArr[i][j] = distance1 + distance2
+
+           
+
+def get_lowest_value(disArr):
+
+    min_value = float('inf')  
+
+ 
+
+    for row in disArr:
+
+        for value in row:
+
+            if value < min_value:
+
+                min_value = value
+
+                column_name = df1.columns[1]
+
+ 
+
+    return min_value
+
+ 
+
+def find_min_value_position(disArr):
+
+    min_value = float('inf')
+
+    min_row = -1
+
+    min_col = -1
+
+ 
+
+    for row_idx, row in enumerate(disArr):
+
+        for col_idx, value in enumerate(row):
+
+            if value < min_value:
+
+                min_value = value
+
+                min_row = row_idx
+
+                min_col = col_idx
+
+ 
+
+    disArr[0][min_col] = 999999
+
+    return min_col
+
+ 
+
+def get_locations(abc):
+
+    data = pd.read_csv("C:/Users/BANU/OneDrive - Sri Lanka Institute of Information Technology/Desktop/Backendtest/Backend_Test/New folder (3)/DataSet.csv")  
+
+    abc.pop(0)
+
+    abc.pop(0)
+
+   
+
+    new_list_size = len(abc)
+
+    abc.pop(new_list_size-1)
+
+   
+
+    new_list_size = len(abc)
+
+    abc.pop(new_list_size-1)
+
+   
+
+    final_start_places = []
+
+   
+
+    for i in range(0, len(abc), 2):
+
+        latitude = abc[i]
+
+        longitude = abc[i + 1]
+
+ 
+
+        # Find matching rows in the DataFrame
+
+        matching_rows = data[(data['Latitude'] == latitude) & (data['Longitude'] == longitude)]
+
+ 
+
+        if not matching_rows.empty:
+
+            Places = matching_rows.iloc[0]['Places']
+
+            final_start_places.append(Places)
+
+        else:
+
+            print(f"No matching place found for Latitude: {latitude}, Longitude: {longitude}")
+
+           
+
+    return final_start_places
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
